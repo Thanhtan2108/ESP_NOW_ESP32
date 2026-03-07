9e1ef855a2d73e87d52cdbbabb4085b1b941bf62
Thanhtan2108 <Nthanhtan2004@gmail.com>
2026-03-07 14:27:49 +0700

    test push button for sender

--- DoorBell/Sender_ESP32/src/main.cpp
+++ DoorBell/Sender_ESP32/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int BUTTON_PIN = 18;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  
+  Serial.println("Test nút nhấn - Nhấn nút để xem kết quả");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  static int lastState = HIGH;
+  int currentState = digitalRead(BUTTON_PIN);
+  
+  if (currentState != lastState) {
+    delay(50); // debounce đơn giản
+    currentState = digitalRead(BUTTON_PIN);
+    
+    if (currentState != lastState) {
+      lastState = currentState;
+      
+      if (currentState == LOW) {
+        Serial.println("Nút NHẤN");
+      } else {
+        Serial.println("Nút THẢ");
+      }
+    }
+  }
 }
